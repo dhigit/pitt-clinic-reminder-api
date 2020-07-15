7c44cc7b410e7f43998cb0809735204a723cb1a6
Yudhi Paramartha <yudhi.chk@gmail.com>
2020-07-16 00:18:46 +0800

    Apply HTTP Cross Origin

--- src/main/java/com/pitt/reminder/api/controller/ApiController.java
+++ src/main/java/com/pitt/reminder/api/controller/ApiController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.pitt.reminder.api.model.TMapping;
 import com.pitt.reminder.api.model.TReminder;
@@ -18,6 +19,7 @@ import com.pitt.reminder.api.repo.ReminderRepo;
 
 @RestController
 @ResponseBody
+@CrossOrigin(origins = "*")
 public class ApiController {
 
 	@Autowired
